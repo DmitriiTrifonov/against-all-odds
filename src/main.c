fd0b0ba0e19a20a40660f3362b899d89dd1de5d9
Dmitrii Trifonov <53140442+DmitriiTrifonov@users.noreply.github.com>
2019-10-07 16:54:45 +0300

    And I've been waiting for this moment for all my life, oh Lord

--- src/main.c
+++ src/main.c
@@ -4,7 +4,8 @@ int main()
 {
     while(1) {
         printf("Please enter a number: ");
-        int n = getchar();
+        int n;
+        scanf("%d", &n);
         printf("\nSo take a look at me now!\n");
         if (n % 2 == 0) {
             printf("%d\n", n);
